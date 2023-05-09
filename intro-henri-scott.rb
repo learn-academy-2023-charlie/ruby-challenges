3fa881c84d8887874f817a6d1ddbadf01cb4b9a3
henrike32 <henrique.lambertucci@gmail.com>
2023-05-09 18:23:44 -0400

    first part henri driver scott navi

---
+++ intro-henri-scott.rb
@@ -0,0 +1,53 @@
+# ❤️ Challenges
+# Remember that floats are fractional numbers whereas integers are whole numbers. In Ruby, 1 and 1.0 are defined by different data types with slightly different behaviors.
+
+# Complete the following challenges in the IRB console.
+
+# Open the IRB terminal. Exit the terminal and reopen the terminal.
+# Add, subtract, multiply, and divide integers.
+> 2 + 2 => 4 ; > 2 - 1 => 1 ; > 32 * 7 => 224 ; > 4 / 2=> 2 
+# Add, subtract, multiply, and divide floats.
+> 2.3 + 4.2 => 6.5; > 3.1-1.2 => 1.90; > 3.5 * 4.3 => 15.049; >  4.0 / 2.1 => 1.90 ; 
+# Find the remainder of dividing two numbers using the modulo operator (%).
+> 32 % 5 => 2;
+# Divide an integer by 0.
+> 1000 / 0 ;  divided by 0 (ZeroDivisionError)
+# Divide a float by 0.
+> 1000.0 / 0 => Infinity
+# Divide 0 by 0.
+> 0 / 0 ;  divided by 0 (ZeroDivisionError) 
+> 0.0 / 0 => NaN 
+# Create a variable and assign an integer.
+new_variable = 32
+# Calculate the variable divided by 2.
+new_variable / 2  => 16
+# Find the remainder of the variable when divided by 3.
+new_variable % 3  => 2
+# Create another variable and assign it the integer 13.
+var_two = 13
+# Use the relational operators on the two variables.
+> new_variable > var_two => true 
+# Reassign the value of one variable to be 7.
+> new_variable = 7 => 7 
+# Reassign the value of one variable to be 26 times its current value.
+> new_variable = 182 => 182
+# Complete the following challenges in a Ruby file.
+
+# Create a variable and return it in a sentence using string interpolation.
+# Create a variable that contains a string and test some of the Ruby string methods:
+# .upcase
+# .reverse
+# .include?
+# .capitalize
+# .delete
+# .index
+# .swapcase
+# Create an array that contains the name of at least five TV shows you enjoy.
+# Find the length of the array.
+# Return the first item in the array.
+# Return the fourth item in the array.
+# Permanently reverse the order of the array.
+# Create a new empty array for your top favorite TV shows.
+# Using the full TV show array, add your top two favorite shows to the empty array.
+
+
