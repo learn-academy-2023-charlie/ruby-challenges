440e5d4521e02c6bfeb0f63e3355b8cf00f51f8e
Justinnkar <jkarpman01@gmail.com>
2023-05-09 15:33:02 -0700

    ruby string methods completed

---
+++ intro-justin-crod-vance.rb
@@ -0,0 +1,29 @@
+# Complete the following challenges in a Ruby file.
+
+# Create a variable and return it in a sentence using string interpolation.
+my_variable = 'Ruby'
+p "This is #{my_variable}"
+puts "This is #{my_variable}"
+puts 'skychocolate'
+# Create a variable that contains a string and test some of the Ruby string methods:
+var_string = 'llamas and kiwis'
+# .upcase
+p var_string.upcase
+# .reverse
+p var_string.reverse
+# .include?
+p var_string.include?('k')
+# .capitalize
+p var_string.capitalize
+# .delete
+p var_string.delete'i'
+# .index
+p var_string.index('i')
+# .swapcase
+# Create an array that contains the name of at least five TV shows you enjoy.
+# Find the length of the array.
+# Return the first item in the array.
+# Return the fourth item in the array.
+# Permanently reverse the order of the array.
+# Create a new empty array for your top favorite TV shows.
+# Using the full TV show array, add your top two favorite shows to the empty array.
