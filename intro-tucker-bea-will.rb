dc33bf11b196a04297eb781adc4bbab428427e2f
wabowen89 <wabowen89@gmail.com>
2023-05-09 15:15:33 -0700

    Completed first 3 challenges

---
+++ intro-tucker-bea-will.rb
@@ -0,0 +1,19 @@
+my_stringvar = 'Charlie'
+
+p "Hello, #{my_stringvar}"
+
+p my_stringvar.upcase
+
+p my_stringvar.reverse
+
+p my_stringvar.include?('a')
+
+p my_stringvar.capitalize
+
+p my_stringvar.delete('e')
+
+p my_stringvar.index('l')
+
+p my_stringvar.swapcase
+
+array = ["South Park", "Barry", "Community", "Game of Thrones", "Parks and Rec"]
