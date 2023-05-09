23d11b1d92e9b00e9754ae679bba2125b081c43e
Aubzzz <aubrey.diehl28@gmail.com>
2023-05-09 15:29:13 -0700

    aubrey's driving

---
+++ intro-aubrey-kyle.rb
@@ -0,0 +1,19 @@
+#Create a variable and return it in a sentence using string interpolation.
+my_name = "aubrey"
+p "Hey there #{my_name}"
+
+#Create a variable that contains a string and test some of the Ruby string methods:
+#.upcase
+#.reverse
+#.include?
+#.capitalize
+#.delete
+#.index
+#.swapcase
+#Create an array that contains the name of at least five TV shows you enjoy.
+#Find the length of the array.
+#Return the first item in the array.
+#Return the fourth item in the array.
+#Permanently reverse the order of the array.
+#Create a new empty array for your top favorite TV shows.
+#Using the full TV show array, add your top two favorite shows to the empty array.
