b776b0c36aa0d887d6a75d72e548f7ddd22e5ac7
wabowen89 <wabowen89@gmail.com>
2023-05-10 10:50:45 -0700

    Completed Rock, Paper, Scissors challenge

--- conditionals-methods-tucker-will.rb
+++ conditionals-methods-tucker-will.rb
@@ -33,3 +33,35 @@
 # end
 
 # p palindrome('tucker')
+
+
+puts 'First player enter your name'
+first_name = gets.chomp
+
+puts 'Second player enter your name'
+second_name = gets.chomp
+
+puts "#{first_name}, please enter one of the following: Rock, Paper, or Scissors"
+first_response = gets.chomp
+
+puts "#{second_name}, please enter one of the following: Rock, Paper, or Scissors"
+second_response = gets.chomp
+
+
+def rps_winner (string1, string2, name1, name2)
+
+    if (string1 == string2)
+        'Tie!!'
+    elsif (string1 == 'rock' && string2 == 'scissors')
+        "#{name1} wins!!!"
+    elsif (string1 == 'scissors' && string2 == 'paper')
+        "#{name1} wins!!!"
+    elsif (string1 == 'paper' && string2 == 'rock')
+        "#{name1} wins!!!"
+    else
+        "#{name2} wins!!!"
+    end
+
+end
+
+p rps_winner(first_response, second_response, first_name, second_name)
