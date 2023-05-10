c5dae1d3882999520796f7a9e6ccdccd3d66e9bc
henrike32 <henrique.lambertucci@gmail.com>
2023-05-10 15:02:48 -0400

    henri driver, scott navigator, lunchtime

---
+++ conditionals-methods-henri-scott.rb
@@ -0,0 +1,73 @@
+# ❤️ Challenges
+
+# Create a method called sum_these_numbers which takes two integers as an argument and prints their sum to the screen.
+
+# def sum_these_numbers (num1, num2)
+#     num1 + num2
+# end
+# p sum_these_numbers(3 , 4)
+# output: 7
+# Create a method called is_even, which takes a single integer, and which then returns true if the number is even, and false otherwise.
+# def is_even  number
+#     if number % 2 == 0
+#         puts "#{number} is Even"
+#     else
+#         puts "#{number} is Odd"
+#     end
+# end
+# p is_even(7)
+# Create a method that takes a number as an argument and prints "Valid" if the number is between 1 and 10 (inclusive) and "Invalid" otherwise.
+# def my_method number
+#     if number < 11
+#     p "true"
+#     else 
+#     p "false"
+#     end
+# end
+# p my_method(0)
+# Create a method that takes in a string and determines if the string is a palindrome.
+
+# def first_pal (string)
+#     if string == string.reverse
+#         "IT IS A PALINDROME!!!"
+#     else "IT IS NOT A PALINDROME, SUCKER!"
+#     end
+# end
+# p first_pal "happy"
+
+# ✂️ Challenge: Rock, Paper, Scissors
+
+# As the first user, I can see a prompt in the terminal to enter my name.
+puts 'enter your name'
+name = gets
+
+
+
+
+# As the second user, I can see a prompt in the terminal to enter my name.
+
+# As the first user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+
+# As the second user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+
+# As a user, I can see a message in the terminal depicting which user won the round.
+
+# As a user, I can see a message in the terminal noting if there was a tie.
+
+
+
+
+# ✅ Challenge: Password Checker
+# As a developer, you have been tasked with creating the user registration for a secure web site. To test your code, enter the user ID and password in the terminal to find out if they are acceptable. The user ID and password must adhere to the to following criteria:
+
+# User ID and password cannot be the same.
+
+# User ID and password must be at least six characters long.
+
+# Password must contain at least one of: !#$
+
+# User ID cannot contain the following characters: !#$ or spaces
+
+# Password cannot be the word "password".
+
+# User password must contain at least one number.
