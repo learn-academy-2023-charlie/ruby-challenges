bcf26571b5a806cf6001c9d087fb52987b13f08f
Tmacintyre <Tucker.w.macintyre@gmail.com>
2023-05-10 13:23:57 -0400

    first challenges

---
+++ conditionals-methods-tucker-will.rb
@@ -0,0 +1,35 @@
+# def sum_these_numbers (num1, num2)
+#     num1 + num2
+# end
+
+# p sum_these_numbers(5, 10)
+
+# def is_even (num)
+#     if num % 2 == 0
+#         'the number is even'
+#     else
+#         'the number is odd'
+#     end
+# end 
+
+# p is_even(2)
+
+# def between_1_10 (num)
+#     if num <= 10 && num >= 1
+#         'valid'
+#     else
+#         'invalid'
+#     end
+# end
+
+# p between_1_10(10)
+
+# def palindrome (string)
+#     if string == string.reverse
+#         'this is a palindrome'
+#     else
+#         'this is not a palindrome'
+#     end
+# end
+
+# p palindrome('tucker')
