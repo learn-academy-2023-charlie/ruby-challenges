d79806ea7e58dd2150a4daff38202112e5bf2cd7
LEARN Academy <lanyuke@hotmail.com>
2023-05-10 10:28:50 -0700

    switching driver after first 4

---
+++ conditionals-methods-yuehan-suri.rb
@@ -0,0 +1,37 @@
+# def sum_these_number (num1, num2)
+# num1 + num2
+# end
+
+# p sum_these_number(5, 7)
+
+# def is_even num
+#     if num % 2 == 0
+#         'true'
+#     else 
+#     'false'
+#     end
+# end
+
+# p is_even(10)
+
+# def valid_num num
+#     if num < 1 || num >= 11 
+#         'Invalid'
+#     else
+#         'Valid'
+#     end
+# end
+
+# p valid_num(5)
+# p valid_num(0)
+# p valid_num(33)
+
+# def str_pal string1
+#     if string1 = string1.reverse
+#         "#{string1} is palindrome string!"
+#     else
+#         "#{string1} is not palindrome string!"
+#     end
+# end
+
+# p str_pal("racecar")
