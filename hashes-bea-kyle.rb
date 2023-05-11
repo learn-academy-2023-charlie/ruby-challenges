37cfda79e0cc2410171752e19a0f6d7e4d68dea6
kyloh1996 <kyloh1996@gmail.com>
2023-05-11 13:55:36 -0500

    tried some online questions

--- hashes-bea-kyle.rb
+++ hashes-bea-kyle.rb
@@ -1,12 +1,12 @@
 # As a developer, I can create a hash called my_phone using the Ruby method .new.
-my_phone = Hash.new
+# my_phone = Hash.new
 
 # As a developer, I can add five key:value pairs of app names and their descriptions to my hash.
-my_phone["camera"] = 'an app to take pictures'
-my_phone["email"] = 'an app to check electronic mail'
-my_phone["music"] = 'an app to listen to songs'
-my_phone["LinkedIn"] = 'an app to network'
-my_phone["photos"] =  'an app to store pictures'
+# my_phone["camera"] = 'an app to take pictures'
+# my_phone["email"] = 'an app to check electronic mail'
+# my_phone["music"] = 'an app to listen to songs'
+# my_phone["LinkedIn"] = 'an app to network'
+# my_phone["photos"] =  'an app to store pictures'
 
 # # As a developer, I can return a value from my_phone by passing in the name of a key.
 # puts my_phone["camera"]
@@ -35,11 +35,11 @@ my_phone["photos"] =  'an app to store pictures'
 
 # As a developer, I can create a custom method that takes in my_phone and returns an array with the app name capitalized and information about each phone app.
 
-def app_info(my_phone) 
-    my_phone.map do |app, descriptions|
-        "#{app.capitalize}: #{descriptions}"
-    end
-end
+# def app_info(my_phone) 
+#     my_phone.map do |app, descriptions|
+#         "#{app.capitalize}: #{descriptions}"
+#     end
+# end
 # p app_info(my_phone)
 
 # As a developer, I can create a custom method that takes in my_phone and returns an array with a sentence about the name of each app.
@@ -57,3 +57,56 @@ end
 #     end
 # end
 # p app_sentences(my_phone)
+
+
+# Level 1
+# 1.
+# Ask user to give name and marks of 5 different students. Store them in hash.
+
+# my_class = Hash.new
+# my_class ['bea'] = 'B'
+# my_class ['scott'] = 'F'
+# my_class ['cherry'] = 'C'
+# my_class ['will'] = 'A'
+# my_class ['kyle'] = 'D'
+
+# p my_class
+# 2.
+# Sort the hash created in previous example according to marks.
+#  def my_grades(my_class) 
+#     my_class.sort_by do |name, grade|
+#         grade
+#         end
+#         end
+        # p my_grades(my_class)
+
+# 3.
+# Use hash to store antonyms of words. E.g.- 'Right'=>'Left', 'Up'=>'Down', etc. Display all words and then ask user to enter a word and display antonym of it.
+# antonyms = {
+#   'Right' => 'Left',
+#   'Up' => 'Down'
+# }
+# antonyms.each do |word, antonyms|
+#     puts "#{word} - #{antonyms}"
+# end
+# print "Enter a word: "
+# word = gets.chomp.capitalize 
+
+# if antonyms.key?(word)
+#     puts "the antonym of #{word} is #{antonyms[word]}"
+# else 
+#     puts "sorry, #{word} is not an antonym"
+# end
+# 4.
+# Count the number of occurrence of each leter in word "MISSISSIPPI". Store count of every leter with the leter in a dictionary.
+
+
+
+
+# 5.
+# From the previous question, sort according to the number of letters.
+# 6.
+# Take an array containg only strings. Now, take a string input from user and rearrange the elements of the list according to the number of occurence of the string taken from user in the elements of the list.
+# E.g.-LIST : ["no bun","bug bun bug bun bug bug","bunny bug","buggy bug bug buggy"]
+# STRING TAKEN : "bug"
+# OUTPUT LIST:["bug bun bug bun bug bug","buggy bug bug buggy","bunny bug","no bun"]
