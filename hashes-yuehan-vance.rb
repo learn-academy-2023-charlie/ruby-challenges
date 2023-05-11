63d1f049273a5aefa4c95b957fbda201b3cc0a7e
m3vance <m3vance@gmail.com>
2023-05-11 11:15:23 -0700

    finished but may play around more since we have a lot of time

--- hashes-yuehan-vance.rb
+++ hashes-yuehan-vance.rb
@@ -9,9 +9,9 @@
 
     my_phone[:facebook] = 'social media platform'
     my_phone[:instagram] = 'social media platform'
-    my_phone[:Youtube] = 'new network media platform'
-    my_phone[:Amazon] = 'shopping platform'
-    my_phone[:Yelp] = 'business review platform'
+    my_phone[:youtube] = 'new network media platform'
+    my_phone[:amazon] = 'shopping platform'
+    my_phone[:yelp] = 'business review platform'
 
 
 # As a developer, I can return a value from my_phone by passing in the name of a key.
@@ -27,21 +27,36 @@
 # As a developer, I can update two values in my_phone.
 
     my_phone[:twitch] = 'streaming platform with many gamers'
-    my_phone[:Amazon] = 'most popular shopping platform'
+    my_phone[:amazon] = 'most popular shopping platform'
  
 
 # As a developer, I can delete two key:value pairs from my_phone.
 
     my_phone.delete(:twitch)
-    my_phone.delete(:Amazon)
+    my_phone.delete(:amazon)
     # p my_phone
 
 # As a developer, I can use an enumerable method to return information about all of my_phone's applications.
 
-    my_phone.each do |key, value|
-        p "#{key} is #{value}" 
-    end 
+    # my_phone.each do |key, value|
+    #     p "#{key} is #{value}" 
+    # end 
     
 # 🏔 Stretch Goals
 # As a developer, I can create a custom method that takes in my_phone and returns an array with the app name capitalized and information about each phone app.
+
+# def key_capitalizer (hash)
+#     hash.map do |key, value|[key.capitalize, value]
+#     end
+# end
+
+# p key_capitalizer(my_phone)
+
 # As a developer, I can create a custom method that takes in my_phone and returns an array with a sentence about the name of each app.
+
+def app_describer (hash)
+     hash.map do |key, value|
+        p "#{key} is a #{value}" 
+    end 
+end
+    p app_describer(my_phone)
