456dea8fdc71f0228b946e3650ede8afe8fe0839
Tmacintyre <Tucker.w.macintyre@gmail.com>
2023-05-11 18:57:01 -0400

    done first challenge some second

--- objects-tucker-scott.rb
+++ objects-tucker-scott.rb
@@ -56,28 +56,61 @@
 
 # As a developer, I can update the status of each instance of class Task when the task has been completed.
 
-class Task
-    def set_title(title)
-        @title = title
-        @status = 'incomplete'
+# class Task
+#     def set_title(title)
+#         @title = title
+#         @status = 'incomplete'
+# end
+#     def get_title
+#         @title 
+#     end
+#     def get_status
+#         @status
+#     end
+#     def complete_task
+#         @status = 'complete'
+#     end
+# end
+
+# chores = Task.new
+# sweeping = Task.new
+# dishes = Task.new
+
+# chores.set_title('chores')
+# sweeping.set_title('sweeping')
+# dishes.set_title('dishes')
+
+# chores.complete_task
+# sweeping.complete_task
+# dishes.complete_task
+# # p chores
+# p sweeping
+# p dishes
+
+
+# For the following ColorPalette challenge use initialize and attr_accessor methods in your class.
+
+# As a developer, I can create a class called ColorPalette.
+class ColorPalette
+
+    def set_color(color)
+        @color = color
+    end
+
+    def get_color
+        @color
+    end
 end
-    def get_title
-        @title 
-    end
-    def get_status
-        @status
-    end
-    def complete_task
-        @status = 'complete'
-    end
-end
 
-chores = Task.new
-sweeping = Task.new
-dishes = Task.new
+green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
+blue = ColorPalette.new('Teal', 'Navy', 'LightBlue')
+red = ColorPalette.new('Maroon', 'Brick' 'Barn')
 
-chores.set_title('chores')
-sweeping.set_title('sweeping')
-dishes.set_title('dishes')
-
-p chores.get_status
+p green.get_color
+# As a developer, I can create three instances (objects) of class ColorPalette.
+# e.g green = ColorPalette.new
+# As a developer, I can initialize each instance of the class ColorPalette with three colors.
+# e.g. green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
+# As a developer, I can print the value of each individual color.
+# As a developer, I can create a method called all_colors that when called will print a sentence telling me the three colors of a given palette.
+# As a developer, I can change one or more colors of a given palette.
