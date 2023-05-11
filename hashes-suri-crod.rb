3b61e9e3880d93e9cbfe46b5e1ab321ee615c916
surielisrodriguez <surielisrodriguez7@gmail.com>
2023-05-11 10:27:42 -0700

    first driver

---
+++ hashes-suri-crod.rb
@@ -0,0 +1,40 @@
+# As a developer, I can create a hash called my_phone using the Ruby method .new.
+my_phone = Hash.new
+p my_phone
+
+# As a developer, I can add five key:value pairs of app names and their descriptions to my hash.
+
+my_phone [:app1] = 'Youtube'
+my_phone [:app2] = 'Twitter'
+my_phone [:app3] = 'Reddit'
+my_phone [:app4] = 'TikTok'
+my_phone [:app5] = 'Instagram'
+
+p my_phone 
+
+
+# As a developer, I can return a value from my_phone by passing in the name of a key.
+
+p my_phone [:app3]
+
+
+# As a developer, I can update two keys in my_phone.
+
+
+
+
+# As a developer, I can update two values in my_phone.
+
+
+# As a developer, I can delete two key:value pairs from my_phone.
+
+
+# As a developer, I can use an enumerable method to return information about all of my_phone's applications.
+
+
+# 🏔 Stretch Goals
+
+# As a developer, I can create a custom method that takes in my_phone and returns an array with the app name capitalized and information about each phone app.
+
+
+# As a developer, I can create a custom method that takes in my_phone and returns an array with a sentence about the name of each app.
