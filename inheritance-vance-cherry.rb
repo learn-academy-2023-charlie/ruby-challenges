b4438bdb8335a6a9d6faeacfabf6d9d29c4a76c4
cherrythefool <sarahandecherry@gmail.com>
2023-05-12 14:08:34 -0600

    last push

--- inheritance-vance-cherry.rb
+++ inheritance-vance-cherry.rb
@@ -8,19 +8,24 @@
 #     * Hint: Use attr_accessor as well as an initialize method.
 
 class Animal
-    attr_accessor(:name, :alive, :age)
-    def initialize(name, alive, age)
-        @name = name
+    attr_accessor(:name, :alive, :age, :death_age)
+    def initialize(name, alive, age, death_age)
+        @name = name.capitalize
         @alive = true
         @age = 0
+        @death_age = death_age
     end
 
     def happy_birthday
         @age += 1
     end
+
+    if @age >= @death_age
+        @alive = false
+    end
 end
 
-# fluffy = Animal.new(true, 0)
+# fluffy = Animal.new('fluffy', true, 0, 20)
 # p fluffy.alive
 
 # ✅ As a developer, I can create a Fish that inherits from Animal.
@@ -28,40 +33,38 @@ end
 
 class Fish < Animal
     attr_accessor(:cold_blooded, :species)
-    def initialize(name, alive, age, cold_blooded, species)
-        super(name, alive, age)
+    def initialize(name, alive, age, death_age, cold_blooded, species)
+        super(name, alive, age, death_age)
         @cold_blooded = true
-        @species = species
+        @species = species.capitalize
     end
 
     def fish_mail
-        "#{name} is a #{age} year old #{species} species. It is #{alive} that it is alive and it is #{cold_blooded} that it is cold blooded."
+        "This fish is a #{age} year old #{species} #{name}. It is #{alive} that it is alive and it is #{cold_blooded} that it is cold blooded."
     end
 end
 
-# * As a developer, I can create a Salmon that inherits from Fish.
+# ✅ As a developer, I can create a Salmon that inherits from Fish.
+# ✅ As a developer, I can initialize my Salmon to be a specific species (Atlantic, Sockeye, etc).
+# ✅ As a developer, I can see that my Salmon is cold-blooded.
 
+salmon = Fish.new('Salmon', true, 0, 4, true, 'Atlantic')
+p salmon.cold_blooded
 
-# * As a developer, I can initialize my Salmon to be a specific species (Atlantic, Sockeye, etc).
-
-salmon = Fish.new('Sal', true, 0, true, 'Atlantic')
-
-
-# * As a developer, I can see that my Salmon is cold-blooded.
-
-# p salmon.cold_blooded
-
-# * As a developer, I can age my Salmon up.
+# ✅ As a developer, I can age my Salmon up.
+# ✅ As a developer, I can see a message that tells me all of my Salmon's information.
 
 p salmon.happy_birthday
 p salmon.fish_mail
 
-# * As a developer, I can see a message that tells me all of my Salmon's information.
-
-
-
 # * As a developer, if my Salmon reaches the ripe old age of 4, I can make it die peacefully after a full and happy life.
 #     * Hint: You will need a method that changes the status of alive in the initialize method of Animal.
+
+salmon.happy_birthday
+salmon.happy_birthday
+salmon.happy_birthday
+p salmon
+
 # * As a developer, I can create a Mammal that inherits from Animal.
 # * As a developer, I can initialize all of my Mammals to be warm_blooded.
 # * As a developer, I can create a Bear that inherits from Mammal.
