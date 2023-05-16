2f02f20609f472e34cc604df1486ef50c7590006
scott198989 <youremail@example.comscott-tuschl@comcast.net>
2023-05-16 13:35:09 -0500

    st commit

--- repec-yuehan-scott-rashaan/rspec.rb
+++ repec-yuehan-scott-rashaan/rspec.rb
@@ -2,12 +2,18 @@
 # Process: Copy the story into your RSpec file. Write the test FIRST. Ensure the test fails correctly. Then write the code that will make the test pass.
 
 # 📚 User Stories
-# As a developer, I can create a Task.
+# As a developer, I can create a Task. 
+
 # As a developer, I can give a Task a title and retrieve it.
+
 # As a developer, I can give a Task a description and retrieve it.
+
 # As a developer, I can mark a Task done. Tasks should be initialized as 'in progress'.
+
 # As a developer, when I print a Task that is done, its status is shown.
+
 # As a developer, I can give a Task a due date. Hint: Use the built-in Ruby Date class.
+
 # 🏔 Stretch Goals
 # As a developer, I can add all of my Tasks to a TaskList.
 # As a developer with a TaskList, I can print the completed items.
