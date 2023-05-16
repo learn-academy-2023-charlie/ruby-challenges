fa11ae598f2625e4c08a3c11604e3d1931b86c61
jricks86 <jricks86@gmail.com>
2023-05-16 15:59:10 -0700

    completed challenges

--- rspec-tucker-jessica/rspec.rb
+++ rspec-tucker-jessica/rspec.rb
@@ -1,13 +1,21 @@
+require 'date'
+
 class Task
-    attr_accessor :title, :description, :status
-    def initialize(title = 'study', description = 'description', status = 'in progress')
-        @title = title
-        @description = description
-        @status = status
-    end
+  attr_accessor :title, :description, :status, :due_date
+
+  # make due_date optional with a default value of nil
+  def initialize(title = 'study', description = 'description', status = 'in progress', due_date = nil)
+    @title = title
+    @description = description
+    @status = status
+
+    # parse the due_date string into a Date object
+    @due_date = Date.parse(due_date) unless due_date.nil?
+  end
 end
 
-laundry = Task.new('laundry', 'wash clothes')
+laundry = Task.new('laundry', 'wash clothes', 'done', '2023-05-25')
 laundry.status = 'done'
 
+# remove the unnecessary laundry.due_date line
 p laundry
--- rspec-tucker-jessica/rspec_spec.rb
+++ rspec-tucker-jessica/rspec_spec.rb
@@ -5,6 +5,9 @@ describe 'Task' do
     it 'has to be real' do
         expect{Task.new}.to_not raise_error
     end
+# Failed examples:
+    # rspec ./rspec_spec.rb:5 # Task has to be real
+
 
     it 'has to have a title' do
         my_task = Task.new
@@ -12,35 +15,42 @@ describe 'Task' do
         expect(my_task.title).to be_a String
     end
     # Failed examples:
-    # rspec ./rspec_spec.rb:5 # Task has to be real
     # rspec ./rspec_spec.rb:9 # Task has to have a title
 
-    # Pass:
-#     Task
-#   has to be real
-#   has to have a title
-
-# Finished in 0.00148 seconds (files took 0.04741 seconds to load)
-# 2 examples, 0 failures
-
+    
     it 'has to have a description' do
-    my_task = Task.new
-    my_task.description = 'description'
-    expect(my_task.description).to eq('description')
-    expect(my_task.description).to be_a String
+        my_task = Task.new
+        my_task.description = 'description'
+        expect(my_task.description).to eq('description')
+        expect(my_task.description).to be_a String
     end
 # Failed examples:
-
 # rspec ./rspec_spec.rb:27 # Task has to have a description
-# Pass:
-# Task
-#   has to be real
-#   has to have a title
-#   has to have a description
+
+
     it 'has to mark a Task done' do
         my_task = Task.new
         my_task.status = 'in progress'
         expect(my_task.status).to eq('in progress')
         expect(my_task.status).to be_a String
     end
+
+    it 'has a due date' do
+        my_task = Task.new
+        my_task.due_date = Date.new(2023, 05, 25)
+        expect(my_task.due_date).to eq(Date.new(2023, 05, 25))
+        expect(my_task.due_date).to be_a Date
+    end
 end  
+
+#<Task:0x0000000103c5e168 @title="laundry", @description="wash clothes", @status="done", @due_date=#<Date: 2023-05-25 ((2460090j,0s,0n),+0s,2299161j)>>
+
+# Task
+#   has to be real
+#   has to have a title
+#   has to have a description
+#   has to mark a Task done
+#   has a due date
+
+# Finished in 0.00146 seconds (files took 0.04818 seconds to load)
+# 5 examples, 0 failures
